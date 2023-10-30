ba3e7bb1e79c4c6d52e35320feed7bebd0426e59
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-10-30 14:09:06 +0200

    Router setup/links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta perspiciatis earum non laudantium, numquam vel, illo ratione asperiores quaerat quidem consectetur enim reprehenderit repellendus molestias neque labore dolor ab sapiente et! Est quidem sed saepe deleniti suscipit placeat. Deleniti, in provident eius, soluta quam sint dolorem perspiciatis nesciunt reiciendis tenetur mollitia ipsum, exercitationem doloremque deserunt ad sequi laborum non. Reiciendis dignissimos omnis provident ullam, nihil voluptatibus cumque neque fugit iure vero recusandae amet sunt dolorum. Natus saepe deserunt consectetur sapiente atque hic voluptate culpa. Quam error illo cupiditate debitis! Quia magnam consequuntur explicabo ut earum laudantium. Minima unde odio commodi!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta perspiciatis earum non laudantium, numquam vel, illo ratione asperiores quaerat quidem consectetur enim reprehenderit repellendus molestias neque labore dolor ab sapiente et! Est quidem sed saepe deleniti suscipit placeat. Deleniti, in provident eius, soluta quam sint dolorem perspiciatis nesciunt reiciendis tenetur mollitia ipsum, exercitationem doloremque deserunt ad sequi laborum non. Reiciendis dignissimos omnis provident ullam, nihil voluptatibus cumque neque fugit iure vero recusandae amet sunt dolorum. Natus saepe deserunt consectetur sapiente atque hic voluptate culpa. Quam error illo cupiditate debitis! Quia magnam consequuntur explicabo ut earum laudantium. Minima unde odio commodi!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
